e448b9227b185e0a1919b21a8d302e9deeae5170
Aakibvah <87711863+Aakibvah@users.noreply.github.com>
2023-02-17 11:39:34 -0500

    adding project

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,18 @@
+import './card.styles.css';
+
+const Card = ({ monster }) => {
+  console.log('in card', monster);
+  const { email, id, name } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
